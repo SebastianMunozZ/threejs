3582848b1e2cf16144eabf399d37d1c134b5bda9
Sebastian <sebastian.munoz.z@gmail.com>
2024-08-27 08:35:26 -0400

    se agrega personaje y animaciones

---
+++ src/models/characters/YBot/Filelist.js
@@ -0,0 +1,9 @@
+const fileList = {
+    "0":"Idle.fbx",
+    "1":"Running.fbx",
+    "2":"Running_Backward.fbx",
+    "3":"Left_Strade.fbx",
+    "4":"Rigth_Strade.fbx",
+}
+
+export default fileList
